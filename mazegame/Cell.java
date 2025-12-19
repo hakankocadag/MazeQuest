99c6e9fdd18748212053a241b13fb1230dc1b64a
Hakan Kocadağ <hakankocadag038@gmail.com>
2025-12-19 20:48:46 +0300

    Update Cell.java

--- mazegame/Cell.java
+++ mazegame/Cell.java
@@ -15,27 +15,4 @@ public class Cell {
         this.row = row;
         this.col = col;
     }
-    
-    public void draw() {
-        if (visited) {
-            StdDraw.setPenColor(StdDraw.WHITE);
-        } else {
-            StdDraw.setPenColor(StdDraw.BLACK);
-        }
-        StdDraw.filledSquare(col + 0.5, row + 0.5, 0.5);
-        
-        StdDraw.setPenColor(StdDraw.RED);
-        if (wallN) {
-            StdDraw.line(col, row + 1, col + 1, row + 1);
-        }
-        if (wallE) {
-            StdDraw.line(col + 1, row, col + 1, row + 1);
-        }
-        if (wallS) {
-            StdDraw.line(col, row, col + 1, row);
-        }
-        if (wallW) {
-            StdDraw.line(col, row, col, row + 1);
-        }
-    }
 }
